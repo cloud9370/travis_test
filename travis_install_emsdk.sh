627a55b20c2fc25441d8b35062ea6f8488135475
cloudchen <cloudchen@softstar.com.tw>
2015-11-25 12:06:49 +0800

    Added travis_install_emsdk.sh
    This script is for install latest emsdk

---
+++ travis_install_emsdk.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+wget https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz
+tar -zvxf emsdk-portable.tar.gz
+cd emsdk-portable
+./emsdk update
+./emsdk install latest
+./emsdk activate latest
