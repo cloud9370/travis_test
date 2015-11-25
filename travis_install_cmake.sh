c10c756ae85bc352639e8714a07d408b3d931c51
cloudchen <cloudchen@softstar.com.tw>
2015-11-25 13:27:27 +0800

    Modify sudo target

--- travis_install_cmake.sh
+++ travis_install_cmake.sh
@@ -3,4 +3,4 @@
 wget --no-check-certificate https://cmake.org/files/v2.8/cmake-2.8.12.2.tar.gz
 tar -zvxf cmake-2.8.12.2.tar.gz
 cd cmake-2.8.12.2
-sudo ./configure && make && make install
+./configure && make && sudo make install
