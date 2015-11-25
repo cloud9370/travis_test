427439d69ed971934646e5739eb09e71083db648
cloud9370 <cloud9370@users.noreply.github.com>
2015-11-25 12:24:23 +0800

    Update travis_install_emsdk.sh

--- travis_install_emsdk.sh
+++ travis_install_emsdk.sh
@@ -2,7 +2,7 @@
 
 wget https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz
 tar -zvxf emsdk-portable.tar.gz
-cd emsdk-portable
+cd emsdk_portable
 ./emsdk update
 ./emsdk install latest
 ./emsdk activate latest
